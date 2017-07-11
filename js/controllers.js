e5e6e246d43f8e8c02d1b01e81dfb5e5fb95815c
Ed <ultralabed@gmail.com>
2017-07-11 10:10:26 +0800

    Refactor

--- js/controllers.js
+++ js/controllers.js
@@ -1,56 +1,31 @@
 /**
  * Created by Ed on 2014/10/8.
  */
-function TodoCtrl($scope){
-    $scope.newItem = "";
-    $scope.todoList = [{ label: "Milk 1 pint" }, { label: "Phone bill" }];
-    $scope.addItem= function(){
-        if(this.newItem) {
-            this.todoList.push({label: this.newItem});
-            this.newItem = "";
-        }
+
+function TodoCrtl($scope){
+  $scope.newItem = "";
+  $scope.todoList = [{ label: "Milk 1 pint" }, { label: "Phone bill" }];
+
+  $scope.addItem= function(){
+    if(this.newItem) {
+      this.todoList.push({label: this.newItem, isFinish:false});
+      this.newItem = "";
     }
+  }
+
+  $scope.removeItem=function(item){
+    item.isFinish = true;
+  }
+
+  $scope.putBackItem=function(item){
+    item.isFinish = false;
+  }
+
+  $scope.edit =function(item){
+    item.editing = true;
+  }
+
+  $scope.save = function(item){
+    delete item.editing;
+  }
 }
-
-function TodoCrtlRemovable($scope){
-    $scope.newItem = "";
-    $scope.todoList = [{ label: "Milk 1 pint" }, { label: "Phone bill" }];
-    $scope.addItem= function(){
-        if(this.newItem) {
-            this.todoList.push({label: this.newItem, isFinish:false});
-            this.newItem = "";
-        }
-    }
-    $scope.removeItem=function(item){
-        item.isFinish = true;
-    }
-    $scope.putBackItem=function(item){
-        item.isFinish = false;
-    }
-}
-
-function TodoCrtlUpdate($scope){
-    $scope.newItem = "";
-    $scope.todoList = [{ label: "Milk 1 pint" }, { label: "Phone bill" }];
-    $scope.addItem= function(){
-        if(this.newItem) {
-            this.todoList.push({label: this.newItem, isFinish:false});
-            this.newItem = "";
-        }
-    }
-    $scope.removeItem=function(item){
-        item.isFinish = true;
-    }
-    $scope.putBackItem=function(item){
-        item.isFinish = false;
-    }
-
-    $scope.edit =function(item){
-        item.editing = true;
-    }
-    $scope.save = function(item){
-        delete item.editing;
-    }
-
-}
-
